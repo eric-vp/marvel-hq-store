ab2a115970190157b7d5072a0c0215b8dbba16f5
Eric <ericvpalavro@gmail.com>
2025-08-07 08:21:25 -0300

    criados GlobalStyled, Header e pagina Home

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+    background-color: #222;
+    color: #fff;
+    padding: 1rem;
+    font-size: 22px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+
+    p {
+        background-color: #be2121;
+        padding: .1rem;
+        color: #fff;
+    }
+`
+
+export default function Header() {
+    return (
+        <StyledHeader>
+            <a>Logo</a>
+            <p>MARVEL HQ STORE</p>
+        </StyledHeader>
+    )
+}
